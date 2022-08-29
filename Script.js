daa164054464f2954aad2a4cb3cc71b0ad69c1a0
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2022-08-29 12:43:10 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,26 @@
+let screen = document.querySelector('#screen');
+btn = document.querySelectorAll('button');
+let screenValue = '';
+for (item of btn) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
